bf94932c1f7efc8cf0018f57a107d8493afe98b7
lukematthews <matthews.luke.c@gmail.com>
2024-02-08 16:21:20 +1100

    Initial implementation of JWT authentication

--- TabcorpTechTest/Controllers/TabcorpTransactionController.cs
+++ TabcorpTechTest/Controllers/TabcorpTransactionController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using TabcorpTechTest.Models.Db;
 using TabcorpTechTest.Models.Dto;
 using TabcorpTechTest.Services;
@@ -9,6 +10,8 @@ namespace TabcorpTechnicalTest.Controllers
     [ApiController]
     [Route("[controller]")]
     [Produces("application/json")]
+    [Authorize]
+
     public class TabcorpTransactionController(ITransactionService transactionService) : ControllerBase
     {
         private readonly ITransactionService transactionService = transactionService;
@@ -21,7 +24,7 @@ namespace TabcorpTechnicalTest.Controllers
         }
 
         // POST api/<TabcorpTransactionController>
-        [HttpPost]
+        [HttpPost, Authorize(Roles ="admin")]
         public IActionResult Post([FromBody] TransactionDto value)
         {
             try
--- TabcorpTechTest/Program.cs
+++ TabcorpTechTest/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Text;
 using System.Text.Json.Serialization;
 using TabcorpTechTest.Data;
 using TabcorpTechTest.Models.Db;
@@ -10,7 +15,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => {
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
 builder.Services.AddDbContext<ApiContext>(opt => opt.UseInMemoryDatabase("TabcorpTechnicalTestDb"));
 builder.Services.AddScoped<ITransactionService, TransactionService>();
 builder.Services.AddSignalR();
@@ -19,13 +32,31 @@ builder.Services.AddControllers()
     {
         options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
     });
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = false,
+        ValidateIssuerSigningKey = true
+    };
+});
+builder.Services.AddAuthorization();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
     using (var scope = app.Services.CreateScope())
     {
         var db = scope.ServiceProvider.GetRequiredService<ApiContext>();
@@ -46,14 +77,20 @@ if (app.Environment.IsDevelopment())
             new Product() { ProductCode = "PRODUCT_004", Cost = 10, Status = TabcorpTechTest.Constants.ProductStatus.Inactive },
             new Product() { ProductCode = "PRODUCT_005", Cost = 500, Status = TabcorpTechTest.Constants.ProductStatus.Active },
         ]);
+
+        db.Users.AddRange([new User() { UserName = "joydip", Password = "joydip123" }]);
         db.SaveChanges();
     }
 }
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-app.MapControllers();
+app.UseAuthentication();
+app.UseAuthorization(); 
+app.MapControllers().RequireAuthorization();
 app.MapHub<TransactionHub>("/transactionHub");
 
 app.Run();
